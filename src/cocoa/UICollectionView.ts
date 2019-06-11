a48a8a7545600969742efc51336e8f975bee40a3
hustlzp <hustlzp@qq.com>
2019-06-11 15:17:11 +0800

    Update.

--- src/cocoa/UICollectionView.ts
+++ src/cocoa/UICollectionView.ts
@@ -10,6 +10,15 @@ export class UICollectionView extends UIView {
     @attribute(String, "Cell Class")
     cellClass: string = "UICollectionViewCell"
 
+    @attribute(Number, "Item Size Width")
+    itemSizeWidth: number = 0
+
+    @attribute(Number, "Line Spacing")
+    minimumLineSpacing: number = 0
+
+    @attribute(Number, "Interitem Spacing")
+    minimumInteritemSpacing: number = 0
+
     // constructor() {
     //     super()
     // }
@@ -21,6 +30,14 @@ export class UICollectionView extends UIView {
         codes += `\n${prefix}delegate = self
 ${prefix}dataSource = self
 ${prefix}register(${this.cellClass || 'UICollectionViewCell'}.self, forCellReuseIdentifier: "${this.cellReuseIdentifier || 'cellReuseIdentifier'}")`
+
+        codes += `\n\nlet layout = UICollectionViewFlowLayout()
+layout.itemSize = CGSize(width: ${this.itemSizeWidth}, height: ${this.itemSizeHeight})
+layout.minimumLineSpacing = ${this.minimumLineSpacing}
+layout.minimumInteritemSpacing = ${this.minimumInteritemSpacing}
+layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+${prefix}collectionViewLayout = layout
+`
 
         return codes
     }
