6dc0c8aa9c5789ddab38b8127ab59d1f7ae0164d
hustlzp <hustlzp@qq.com>
2019-06-15 15:48:08 +0800

    Support UITableViewHeaderFooterView.

--- src/cocoa/UITableViewHeaderFooterView.ts
+++ src/cocoa/UITableViewHeaderFooterView.ts
@@ -1,8 +1,15 @@
 import { UIColor, UIView, UIFont, attribute } from './UIView';
+import { indent } from '@/utils';
 
 export class UITableViewHeaderFooterView extends UIView {
     name: string = "tableHeaderFooterView"
     className: string = "UITableViewHeaderFooterView"
     hasContentView = true
 
+    classInitCodes(): string {
+        let codes = "override init(reuseIdentifier: String?) {"
+        codes += indent("\nsuper.init(reuseIdentifier: reuseIdentifier)")
+
+        return codes
+    }
 }
